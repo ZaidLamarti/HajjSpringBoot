4ea256843105df597b78b1009f97572fb40efc37
Zaid Lamarti <zaid.lamarti.21@gmail.com>
2020-05-17 22:40:28 +0000

    CP

---
+++ src/main/java/com/main/domain/Candidat.java
@@ -0,0 +1,40 @@
+package com.main.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "candidat")
+public class Candidat {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 20)
+	private String nom;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	/*
+	 * @ManyToMany(fetch = FetchType.LAZY)
+	 * 
+	 * @JoinTable( name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+	 * inverseJoinColumns = @JoinColumn(name = "role_id")) private Set<Role> roles =
+	 * new HashSet<>();
+	 */
+}
---
+++ src/main/java/com/main/repository/CandidatRepository.java
@@ -0,0 +1,20 @@
+package com.main.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.main.domain.Candidat;
+
+@Repository
+public interface CandidatRepository extends JpaRepository<Candidat, Long> {
+
+	/*
+	 * Optional<Candidat> findByUsername(String username);
+	 * 
+	 * Boolean existsByUsername(String username);
+	 * 
+	 * Boolean existsByEmail(String email);
+	 */
+}
